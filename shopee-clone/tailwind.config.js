fafce8dddc838fe627acc6a25148ba48b5fd21e9
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-10-01 22:10:12 +0300

    custom class .container with tailwind plugin

--- shopee-clone/tailwind.config.js
+++ shopee-clone/tailwind.config.js
@@ -1,3 +1,5 @@
+import plugin from 'tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -8,5 +10,17 @@ export default {
       }
     }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      });
+    })
+  ]
 };
