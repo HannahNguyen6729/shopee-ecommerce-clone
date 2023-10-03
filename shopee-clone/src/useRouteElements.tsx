78fd74f2d728cb8eca1916bf19f6de23b1f37f1b
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-10-03 23:34:55 +0300

    setup ProtectedRoute, RejectedRoute in react router

--- shopee-clone/src/useRouteElements.tsx
+++ shopee-clone/src/useRouteElements.tsx
@@ -1,14 +1,20 @@
-import { useRoutes } from 'react-router-dom';
+import { Navigate, Outlet, useRoutes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import ProductList from './pages/ProductList/ProductList';
 import RegisterLayout from './layouts/RegisterLayout/RegisterLayout';
 import MainLayout from './layouts/MainLayout/MainLayout';
+import Profile from './pages/Profile/Profile';
+
+const isAuthenticated = false;
+const ProtectedRoute = () => (isAuthenticated ? <Outlet /> : <Navigate to='/login' />);
+const RejectedRoute = () => (isAuthenticated ? <Navigate to='/' /> : <Outlet />);
 
 const useRouteElements = () => {
   const routeElement = useRoutes([
     {
-      path: '/',
+      path: '',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
@@ -16,20 +22,40 @@ const useRouteElements = () => {
       )
     },
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ]);
 
