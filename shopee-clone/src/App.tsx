fe943c9e3d15bfbb1e7482c77e67da41ffe8c9b7
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-10-03 20:18:37 +0300

    handle basic errors with  axios interceptor, react-toastify

--- shopee-clone/src/App.tsx
+++ shopee-clone/src/App.tsx
@@ -1,11 +1,29 @@
 import useRouteElements from './useRouteElements';
+import { ToastContainer } from 'react-toastify';
 
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const routeElements = useRouteElements();
 
-  return <> {routeElements} </>;
+  return (
+    <>
+      {routeElements}
+      <ToastContainer
+        position='top-right'
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
+    </>
+  );
 }
 
 export default App;
--- shopee-clone/src/utils/http.ts
+++ shopee-clone/src/utils/http.ts
@@ -1,7 +1,35 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance, HttpStatusCode } from 'axios';
+import { toast } from 'react-toastify';
 
 export const http: AxiosInstance = axios.create({
   baseURL: 'https://api-ecom.duthanhduoc.com/',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 });
+
+// Add a response interceptor
+http.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error: AxiosError) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+      toast.error(error.message, {
+        position: 'top-right',
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light'
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
