f095a604a5f226daf567e0c965f35cd1da627244
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-10-10 00:43:59 +0300

    refactor code

--- shopee-clone/src/components/Header/Header.tsx
+++ shopee-clone/src/components/Header/Header.tsx
@@ -58,79 +58,81 @@ export default function Header() {
             </div>
           </form>
           <div className='col-span-1 justify-self-end'>
-            <Popover
-              renderPopover={
-                <div className='relative  max-w-[400px] rounded-sm border border-gray-200 bg-white text-sm shadow-md'>
-                  {purchasesInCart && purchasesInCart.length > 0 ? (
-                    <div className='p-2'>
-                      <div className='capitalize text-gray-400'>Recently Added Products</div>
-                      <div className='mt-5'>
-                        {isAuthenticated ? (
-                          purchasesInCart.slice(0, MAX_PURCHASES).map((purchase) => (
-                            <div className='mt-2 flex py-2 hover:bg-gray-100' key={purchase._id}>
-                              <div className='flex-shrink-0'>
-                                <img
-                                  src={purchase.product.image}
-                                  alt={purchase.product.name}
-                                  className='h-11 w-11 object-cover'
-                                />
+            <Link to='/cart'>
+              <Popover
+                renderPopover={
+                  <div className='relative  max-w-[400px] rounded-sm border border-gray-200 bg-white text-sm shadow-md'>
+                    {purchasesInCart && purchasesInCart.length > 0 ? (
+                      <div className='p-2'>
+                        <div className='capitalize text-gray-400'>Recently Added Products</div>
+                        <div className='mt-5'>
+                          {isAuthenticated ? (
+                            purchasesInCart.slice(0, MAX_PURCHASES).map((purchase) => (
+                              <div className='mt-2 flex py-2 hover:bg-gray-100' key={purchase._id}>
+                                <div className='flex-shrink-0'>
+                                  <img
+                                    src={purchase.product.image}
+                                    alt={purchase.product.name}
+                                    className='h-11 w-11 object-cover'
+                                  />
+                                </div>
+                                <div className='ml-2 flex-grow overflow-hidden'>
+                                  <div className='truncate'>{purchase.product.name}</div>
+                                </div>
+                                <div className='ml-2 flex-shrink-0'>
+                                  <span className='text-orange'>${formatCurrency(purchase.product.price)}</span>
+                                </div>
                               </div>
-                              <div className='ml-2 flex-grow overflow-hidden'>
-                                <div className='truncate'>{purchase.product.name}</div>
-                              </div>
-                              <div className='ml-2 flex-shrink-0'>
-                                <span className='text-orange'>${formatCurrency(purchase.product.price)}</span>
-                              </div>
-                            </div>
-                          ))
-                        ) : (
-                          <></>
-                        )}
-                      </div>
-                      <div className='mt-6 flex items-center justify-between'>
-                        <div className='text-xs capitalize text-gray-500'>
-                          {purchasesInCart.length > MAX_PURCHASES ? purchasesInCart.length - MAX_PURCHASES : ''}{' '}
-                          Products In Cart
+                            ))
+                          ) : (
+                            <></>
+                          )}
                         </div>
-                        <Link
-                          to={path.cart}
-                          className='rounded-sm bg-orange px-4 py-2 capitalize text-white hover:bg-opacity-90'
-                        >
-                          View My Shopping Cart
-                        </Link>
+                        <div className='mt-6 flex items-center justify-between'>
+                          <div className='text-xs capitalize text-gray-500'>
+                            {purchasesInCart.length > MAX_PURCHASES ? purchasesInCart.length - MAX_PURCHASES : ''}{' '}
+                            Products In Cart
+                          </div>
+                          <Link
+                            to={path.cart}
+                            className='rounded-sm bg-orange px-4 py-2 capitalize text-white hover:bg-opacity-90'
+                          >
+                            View My Shopping Cart
+                          </Link>
+                        </div>
                       </div>
-                    </div>
-                  ) : (
-                    <div className='flex h-[300px] w-[300px] flex-col items-center justify-center p-2'>
-                      <img src={noproduct} alt='no purchase' className='h-24 w-24' />
-                      <div className='mt-3 capitalize'>No Products Yet</div>
-                    </div>
+                    ) : (
+                      <div className='flex h-[300px] w-[300px] flex-col items-center justify-center p-2'>
+                        <img src={noproduct} alt='no purchase' className='h-24 w-24' />
+                        <div className='mt-3 capitalize'>No Products Yet</div>
+                      </div>
+                    )}
+                  </div>
+                }
+              >
+                <Link to='/' className='relative'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='h-8 w-8'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+                    />
+                  </svg>
+                  {purchasesInCart && purchasesInCart.length > 0 && (
+                    <span className='absolute top-[-5px] left-[17px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange '>
+                      {purchasesInCart?.length}
+                    </span>
                   )}
-                </div>
-              }
-            >
-              <Link to='/' className='relative'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='h-8 w-8'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
-                  />
-                </svg>
-                {purchasesInCart && purchasesInCart.length > 0 && (
-                  <span className='absolute top-[-5px] left-[17px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange '>
-                    {purchasesInCart?.length}
-                  </span>
-                )}
-              </Link>
-            </Popover>
+                </Link>
+              </Popover>
+            </Link>
           </div>
         </div>
       </div>
