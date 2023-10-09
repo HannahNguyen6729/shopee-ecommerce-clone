5f2db37df5881a4923c35b5ec062b8f41fee3a7c
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-10-09 16:03:51 +0300

    handle rating logic

--- shopee-clone/src/components/AsideFilter/AsideFilter.tsx
+++ shopee-clone/src/components/AsideFilter/AsideFilter.tsx
@@ -10,6 +10,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { ObjectSchema } from 'yup';
 import { NoUndefinedField } from '../../types/util.type';
 import { InputSchema, inputSchema } from '../../utils/inputSchema';
+import { omit } from 'lodash';
+import RatingStars from '../RatingStars/RatingStars';
 
 type Props = {
   queryConfig: QueryConfig;
@@ -50,6 +52,10 @@ const AsideFilter = ({ queryConfig, categories }: Props) => {
 
   const handleRemoveAll = () => {
     reset();
+    navigate({
+      pathname: path.home,
+      search: createSearchParams(omit(queryConfig, ['price_min', 'price_max', 'rating_filter', 'category'])).toString()
+    });
   };
 
   return (
@@ -80,7 +86,7 @@ const AsideFilter = ({ queryConfig, categories }: Props) => {
         {categories.map((item) => {
           const isActive = queryConfig.category === item._id;
           return (
-            <li key={item._id} className='py-2 pl-2'>
+            <li key={item._id} className='py-2 pl-2 text-left'>
               <Link
                 to={{
                   pathname: path.home,
@@ -181,32 +187,8 @@ const AsideFilter = ({ queryConfig, categories }: Props) => {
       </div>
       <div className='my-4 h-[1px] bg-gray-300' />
       <div className='text-sm'>Rating</div>
-      {/* TODO: Rating stars */}
-      <svg viewBox='0 0 9.5 8' className='mr-1 h-4 w-4'>
-        <defs>
-          <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-            <stop offset={0} stopColor='#ffca11' />
-            <stop offset={1} stopColor='#ffad27' />
-          </linearGradient>
-          <polygon
-            id='ratingStar'
-            points='14.910357 6.35294118 12.4209136 7.66171903 12.896355 4.88968305 10.8823529 2.92651626 13.6656353 2.52208166 14.910357 0 16.1550787 2.52208166 18.9383611 2.92651626 16.924359 4.88968305 17.3998004 7.66171903'
-          />
-        </defs>
-        <g fill='url(#ratingStarGradient)' fillRule='evenodd' stroke='none' strokeWidth={1}>
-          <g transform='translate(-876 -1270)'>
-            <g transform='translate(155 992)'>
-              <g transform='translate(600 29)'>
-                <g transform='translate(10 239)'>
-                  <g transform='translate(101 10)'>
-                    <use stroke='#ffa727' strokeWidth='.5' xlinkHref='#ratingStar' />
-                  </g>
-                </g>
-              </g>
-            </g>
-          </g>
-        </g>
-      </svg>
+      {/* Rating stars */}
+      <RatingStars queryConfig={queryConfig} />
       <div className='my-4 h-[1px] bg-gray-300' />
       <Button
         onClick={handleRemoveAll}
