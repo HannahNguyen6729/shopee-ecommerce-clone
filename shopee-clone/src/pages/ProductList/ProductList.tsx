f290335f83b4e6115dbc5cddec4c2c505fe18c26
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-10-10 01:08:45 +0300

    improve UI/UX

--- shopee-clone/src/pages/ProductList/ProductList.tsx
+++ shopee-clone/src/pages/ProductList/ProductList.tsx
@@ -13,25 +13,27 @@ const ProductList = () => {
   const { data: categoriesData } = useCategories();
 
   return (
-    <div className='container'>
-      {productsData && (
-        <div className='grid grid-cols-12 gap-6'>
-          <div className='col-span-3'>
-            <AsideFilter queryConfig={queryParams} categories={categoriesData?.data || []} />
-          </div>
-          <div className='col-span-9'>
-            <SortProductList queryConfig={queryParams} pageSize={productsData.data.pagination.page_size} />
-            <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-              {productsData.data.products.map((item) => (
-                <div key={item._id} className='col-span-1'>
-                  <Product product={item} />
-                </div>
-              ))}
+    <div className='bg-gray-200 py-6'>
+      <div className='container'>
+        {productsData && (
+          <div className='grid grid-cols-12 gap-6'>
+            <div className='col-span-3'>
+              <AsideFilter queryConfig={queryParams} categories={categoriesData?.data || []} />
             </div>
-            <Pagination queryConfig={queryParams} pageSize={productsData.data.pagination.page_size} />
+            <div className='col-span-9'>
+              <SortProductList queryConfig={queryParams} pageSize={productsData.data.pagination.page_size} />
+              <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+                {productsData.data.products.map((item) => (
+                  <div key={item._id} className='col-span-1'>
+                    <Product product={item} />
+                  </div>
+                ))}
+              </div>
+              <Pagination queryConfig={queryParams} pageSize={productsData.data.pagination.page_size} />
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
--- shopee-clone/tailwind.config.js
+++ shopee-clone/tailwind.config.js
@@ -1,8 +1,13 @@
-import plugin from 'tailwindcss';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
+
   theme: {
     extend: {
       colors: {
@@ -21,6 +26,7 @@ export default {
           paddingRight: theme('spacing.4')
         }
       });
-    })
+    }),
+    require('@tailwindcss/line-clamp')
   ]
 };
